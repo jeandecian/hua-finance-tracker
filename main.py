f28649a23e2515b3e044b1435548304a432f8fac
Jean Decian <jeandecian@hotmail.fr>
2025-10-02 17:17:13 -0400

    add extracted text save to TXT file

--- main.py
+++ main.py
@@ -12,7 +12,12 @@ def extract_text_from_pdf(pdf_path):
     return text
 
 
+def save_text_to_file(text, file_path):
+    with open(file_path, "w") as text_file:
+        text_file.write(text)
+
+
 if __name__ == "__main__":
     pdf_path = argv[1]
     extracted_text = extract_text_from_pdf(pdf_path)
-    print(extracted_text)
+    save_text_to_file(extracted_text, pdf_path.split(".")[0] + ".txt")
